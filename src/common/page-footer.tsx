0f186220cfc16ce068aff9e7fa269a6b67badc76
Bao D <baocancode@gmail.com>
2021-02-20 14:42:36 +1100

    chore: increase footer padding

--- src/common/page-footer.tsx
+++ src/common/page-footer.tsx
@@ -6,7 +6,7 @@ import { uiConstants } from '@/common/ui-constants'
 export const PageFooter: FunctionComponent = () => {
   return (
     <footer className='has-background-light mt-6'>
-      <ContentBox className='py-6'>
+      <ContentBox>
         <strong className='ml-1'>{uiConstants.appName}</strong>
       </ContentBox>
     </footer>
