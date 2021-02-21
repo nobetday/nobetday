e8401ab9cee942d67562bd12273421a8c6b61ab0
Bao D <baocancode@gmail.com>
2021-02-21 12:18:29 +1100

    chore: increase coming soon size

--- src/common/coming-soon.tsx
+++ src/common/coming-soon.tsx
@@ -5,7 +5,9 @@ import { ContentBox } from '@/common/content-box'
 export const ComingSoon: FunctionComponent = () => {
   return (
     <ContentBox>
-      <div className='notification is-info'>COMING SOON</div>
+      <div className='notification is-info'>
+        <p className='is-size-5'>COMING SOON</p>
+      </div>
     </ContentBox>
   )
 }
