ba2b2920f168ca25df564ce7a54cc0f11aa369af
Bao D <baocancode@gmail.com>
2021-02-26 22:15:02 +1100

    feat: add social links

--- src/common/page-footer.tsx
+++ src/common/page-footer.tsx
@@ -1,3 +1,5 @@
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { FunctionComponent } from 'react'
 
 import { ContentBox } from '@/common/content-box'
@@ -7,7 +9,17 @@ export const PageFooter: FunctionComponent = () => {
   return (
     <footer>
       <ContentBox>
-        <strong className='is-size-4 ml-1'>{uiConstants.appName}</strong>
+        <div className='block'>
+          <strong className='is-size-4 ml-1'>{uiConstants.appName}</strong>
+        </div>
+        <div className='block'>
+          <a href='https://github.com/nobetday/nobetday' className='icon is-large'>
+            <FontAwesomeIcon icon={faGithub} size='2x' />
+          </a>
+          <a href='https://twitter.com/nobetday' className='icon is-large'>
+            <FontAwesomeIcon icon={faTwitter} size='2x' />
+          </a>
+        </div>
       </ContentBox>
     </footer>
   )
