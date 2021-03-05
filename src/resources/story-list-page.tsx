452e589e5b158afc0eb9f21ebbf8b79923b90e1d
Minh Chau <nmchau87@gmail.com>
2021-03-06 00:31:12 +0900

    feat: create selected story modal

--- src/resources/story-list-page.tsx
+++ src/resources/story-list-page.tsx
@@ -1,8 +1,12 @@
 import { GetStaticProps, NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 import { ContentBox } from '@/common/content-box'
 import { PageLayout } from '@/common/page-layout'
-import { getFeaturedStory, getOtherStories, getStories, Story } from '@/resources/story-data'
+import { PageModal } from '@/common/page-modal'
+import { getQueryValue } from '@/common/query'
+import { getFeaturedStory, getOtherStories, getStories, getStoryById, Story } from '@/resources/story-data'
 import { StoryDisplay } from '@/resources/story-display'
 
 export interface StoryListPageProps {
@@ -11,9 +15,22 @@ export interface StoryListPageProps {
 }
 
 export const StoryListPage: NextPage<StoryListPageProps> = ({ featuredStory, otherStories }) => {
+  const router = useRouter()
+  const [selectedStory, setSelectedStory] = useState<Story>()
+  useEffect(() => {
+    const id = getQueryValue(router.query.id)
+    if (id) {
+      setSelectedStory(getStoryById(id))
+    }
+  }, [router])
   return (
     <PageLayout title='Stories'>
       <ContentBox>
+        {selectedStory && (
+          <PageModal>
+            <StoryDisplay story={selectedStory} isFeatured={selectedStory.id === featuredStory.id} />
+          </PageModal>
+        )}
         <h2 className='subtitle is-2'>Featured Story</h2>
         <div className='columns is-centered'>
           <div className='column is-two-thirds'>
