32b9a7caccd197bb061c93f84668bc5013777b86
Minh Chau <nmchau87@gmail.com>
2021-03-05 20:25:23 +0900

    chore: rename function

--- src/common/page-modal.tsx
+++ src/common/page-modal.tsx
@@ -3,13 +3,13 @@ import { FunctionComponent, useState } from 'react'
 
 export const PageModal: FunctionComponent = ({ children }) => {
   const [active, setActive] = useState(true)
-  const onClose = () => setActive(false)
+  const handleClose = () => setActive(false)
 
   return (
     <div className={clsx('modal', active && 'is-active')}>
-      <div className='modal-background' onClick={onClose}></div>
+      <div className='modal-background' onClick={handleClose}></div>
       <div className='modal-content'>{children}</div>
-      <button className='modal-close is-large' onClick={onClose} aria-label='close'></button>
+      <button className='modal-close is-large' onClick={handleClose} aria-label='close'></button>
     </div>
   )
 }
