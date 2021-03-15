64483ab4efe043ce63a87938d165581efc923857
Chau Nguyen <nmchau87@gmail.com>
2021-03-15 23:10:35 +0900

    feat: create share button for story (#12)

--- src/resources/quote-display.tsx
+++ src/resources/quote-display.tsx
@@ -18,11 +18,21 @@ export const QuoteDisplay: FunctionComponent<QuoteDisplayProps> = ({ quote, isFe
     <article className={clsx('message', isFeatured ? 'is-info is-large' : 'is-dark is-medium')}>
       <div className='message-header'>
         <p>{quote.author}</p>
-        <a onClick={handleCopy} className='icon is-small'>
-          <FontAwesomeIcon icon={faCopy} />
-        </a>
       </div>
-      <div className='message-body is-size-4'>{quote.content}</div>
+      <div className='message-body is-size-4'>
+        <p>{quote.content}</p>
+        <div className='level mt-4'>
+          <div className='level-left'>
+            <div className='level-item'>
+              <button onClick={handleCopy} className='button'>
+                <span className='icon is-small'>
+                  <FontAwesomeIcon icon={faCopy} />
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </article>
   )
 }
