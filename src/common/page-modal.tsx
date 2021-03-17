c63dac1c6f67ada890fbae25de763eb8f4e4f904
Bao D <baocancode@gmail.com>
2021-03-17 20:19:47 +1100

    fix: disable page scroll when modal is active

--- src/common/page-modal.tsx
+++ src/common/page-modal.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import { FunctionComponent, useState } from 'react'
+import { FunctionComponent, useEffect, useState } from 'react'
 
 export interface PageModalProps {
   readonly onClose?: () => void
@@ -14,10 +14,18 @@ export const PageModal: FunctionComponent<PageModalProps> = ({ children, onClose
     }
   }
 
+  useEffect(() => {
+    if (active) {
+      document.documentElement.classList.add('is-clipped')
+    } else {
+      document.documentElement.classList.remove('is-clipped')
+    }
+  }, [active])
+
   return (
     <div className={clsx('modal', active && 'is-active')}>
       <div className='modal-background' onClick={handleClose}></div>
-      <div className='modal-content has-background-body p-3'>{children}</div>
+      <div className='modal-content has-background-body p-5'>{children}</div>
       <button className='modal-close is-large' onClick={handleClose} aria-label='close'></button>
     </div>
   )
