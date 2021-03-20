3b89326d395992363e05647f719e2788e7709e99
Bao D <baocancode@gmail.com>
2021-03-20 22:38:12 +1100

    feat: enable plausible analytics

--- src/common/page-head.tsx
+++ src/common/page-head.tsx
@@ -3,6 +3,8 @@ import { FunctionComponent } from 'react'
 
 import { uiConstants } from '@/common/ui-constants'
 
+const plausibleDomain = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN
+
 export interface PageHeadProps {
   readonly title: string
 }
@@ -11,6 +13,9 @@ export const PageHead: FunctionComponent<PageHeadProps> = ({ title }) => {
   return (
     <NextHead>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
+      {!!plausibleDomain && (
+        <script async defer data-domain={plausibleDomain} src='https://plausible.io/js/plausible.js'></script>
+      )}
       <title>
         {title} - {uiConstants.appName}
       </title>
