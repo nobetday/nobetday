8984d1aed036101421f1e371e122dca6edd41c91
Bao D <baocancode@gmail.com>
2021-03-21 22:54:15 +1100

    chore: refactor footer

--- src/common/page-footer.tsx
+++ src/common/page-footer.tsx
@@ -1,10 +1,34 @@
 import { faGithubSquare, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
-import { faChartLine } from '@fortawesome/free-solid-svg-icons'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import clsx from 'clsx'
 import { FunctionComponent } from 'react'
 
-import { uiConstants } from './ui-constants'
+import { uiConstants } from '@/common/ui-constants'
+
+const SocialLinks: FunctionComponent = () => {
+  return (
+    <div className='block'>
+      <div className='field is-grouped is-grouped-centered'>
+        <div className='control'>
+          <a href='https://github.com/nobetday/nobetday' title='GitHub' className='icon is-large'>
+            <FontAwesomeIcon icon={faGithubSquare} size='2x' />
+          </a>
+        </div>
+        <div className='control'>
+          <a href='https://twitter.com/nobetday' title='Twitter' className='icon is-large'>
+            <FontAwesomeIcon icon={faTwitterSquare} size='2x' />
+          </a>
+        </div>
+        <div className='control'>
+          <a href='https://plausible.io/nobetday.com' title='Analytics' className='icon is-large'>
+            <FontAwesomeIcon icon={faGlobe} size='2x' />
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export interface PageFooterProps {
   readonly className?: string
@@ -16,25 +40,7 @@ export const PageFooter: FunctionComponent<PageFooterProps> = ({ className }) =>
       <div className='block'>
         <strong className='is-size-5'>Make every day your {uiConstants.appName}.</strong>
       </div>
-      <div className='block'>
-        <div className='field is-grouped is-grouped-centered'>
-          <div className='control'>
-            <a href='https://github.com/nobetday/nobetday' className='icon is-large'>
-              <FontAwesomeIcon icon={faGithubSquare} size='2x' />
-            </a>
-          </div>
-          <div className='control'>
-            <a href='https://twitter.com/nobetday' className='icon is-large'>
-              <FontAwesomeIcon icon={faTwitterSquare} size='2x' />
-            </a>
-          </div>
-          <div className='control'>
-            <a href='https://plausible.io/nobetday.com' className='icon is-large'>
-              <FontAwesomeIcon icon={faChartLine} size='2x' />
-            </a>
-          </div>
-        </div>
-      </div>
+      <SocialLinks />
     </footer>
   )
 }
