7d6ab375981fe0fc56c86c63fbf6b5a55cb97558
Bao D <baocancode@gmail.com>
2021-03-28 22:27:12 +1100

    feat: show sign in alert

--- src/user/auth-ready.tsx
+++ src/user/auth-ready.tsx
@@ -2,18 +2,42 @@ import { faCompass } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { FunctionComponent } from 'react'
 
-import { useAuthState } from '@/user/auth-context'
+import { useAuthActions, useAuthState } from '@/user/auth-context'
 
-export const AuthReady: FunctionComponent = ({ children }) => {
-  const { isReady } = useAuthState()
-
-  if (isReady) {
-    return <>{children}</>
-  }
+const SignInAlert: FunctionComponent = () => {
+  const { signIn } = useAuthActions()
 
   return (
-    <div className='block has-text-grey'>
-      <FontAwesomeIcon icon={faCompass} size='4x' spin />
+    <div className='notification is-info'>
+      <p className='is-size-4'>
+        Please{' '}
+        <button onClick={signIn} className='button'>
+          SIGN IN
+        </button>{' '}
+        to continue.
+      </p>
     </div>
   )
+}
+
+export interface AuthReadyProps {
+  readonly isAlertEnabled?: boolean
+}
+
+export const AuthReady: FunctionComponent<AuthReadyProps> = ({ isAlertEnabled = false, children }) => {
+  const { isReady, user } = useAuthState()
+
+  if (!isReady) {
+    return (
+      <div className='block has-text-grey has-text-centered'>
+        <FontAwesomeIcon icon={faCompass} size='4x' spin />
+      </div>
+    )
+  }
+
+  if (!user && isAlertEnabled) {
+    return <SignInAlert />
+  }
+
+  return <>{children}</>
 }
