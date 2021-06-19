e0d848a56d7b7aa30796c27e5d07278b7676be4c
Bao D <baocancode@gmail.com>
2021-06-20 08:30:38 +1000

    chore: rename constant

--- src/common/page-head.tsx
+++ src/common/page-head.tsx
@@ -6,7 +6,7 @@ import { FunctionComponent, memo } from 'react'
 // Fix huge icon flash https://github.com/FortAwesome/react-fontawesome/issues/284
 fontawesomeConfig.autoAddCss = true
 
-const plausibleDomain = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN
+const PLAUSIBLE_DOMAIN = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN
 
 export const PageHead: FunctionComponent = memo(() => {
   return (
@@ -15,7 +15,7 @@ export const PageHead: FunctionComponent = memo(() => {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <style type='text/css'>{fontawesomeDom.css()}</style>
       </NextHead>
-      {!!plausibleDomain && <NextScript src='https://plausible.io/js/plausible.js' data-domain={plausibleDomain} />}
+      {!!PLAUSIBLE_DOMAIN && <NextScript src='https://plausible.io/js/plausible.js' data-domain={PLAUSIBLE_DOMAIN} />}
     </>
   )
 })
